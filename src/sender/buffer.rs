855812d87f906ee2dfc54e8100f7bc7cd61573cb
Michael Humphris <hello@michaelhumphris.com>
2023-02-01 21:39:15 -0800

    Fix PulsedataBuffer Reading Past Offset

--- src/sender/buffer.rs
+++ src/sender/buffer.rs
@@ -23,7 +23,7 @@ impl<const S: usize> PulsedataBuffer<S> {
     }
 
     pub fn get(&self, index: usize) -> Option<u32> {
-        self.buf.get(index).cloned()
+        self.buffer().get(index).cloned()
     }
 
     pub fn buffer(&self) -> &[u32] {
@@ -58,6 +58,100 @@ impl<'a> Iterator for PulseIterator<'a> {
             let r = self.pulses[self.pos];
             self.pos += 1;
             Some(r)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate alloc;
+    use alloc::vec::Vec;
+
+    use super::*;
+    use crate::Protocol;
+
+    #[test]
+    fn pulsedata_buffer_with_single_value() {
+        let mut buffer = PulsedataBuffer::<8>::new();
+
+        let data = vec![1];
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(1, buffer.buffer().len());
+
+        let returned_val = buffer.get(0);
+        assert_eq!(Some(1), returned_val);
+    }
+
+    #[test]
+    fn pulsedata_buffer_with_multiple_values() {
+        let mut buffer = PulsedataBuffer::<8>::new();
+
+        let data = vec![0, 1, 2];
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(3, buffer.buffer().len());
+
+        for (idx, &val) in data.iter().enumerate() {
+            assert_eq!(Some(val), buffer.get(idx))
+        }
+    }
+
+    #[test]
+    fn pulsedata_buffer_with_multiple_writes() {
+        let mut buffer = PulsedataBuffer::<8>::new();
+
+        let data = vec![0, 1, 2];
+
+        buffer.load::<StaticEncoder, 0>(&data);
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(6, buffer.buffer().len());
+
+        for (idx, &val) in data.iter().chain(data.iter()).enumerate() {
+            assert_eq!(Some(val), buffer.get(idx))
+        }
+    }
+
+    #[test]
+    fn pulsedata_buffer_get_out_of_bounds() {
+        let mut buffer = PulsedataBuffer::<8>::new();
+
+        let data = vec![0];
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(None, buffer.get(1));
+    }
+
+    #[test]
+    fn pulsedata_buffer_reset() {
+        let mut buffer = PulsedataBuffer::<8>::new();
+
+        let data = vec![0];
+
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(Some(0), buffer.get(0));
+
+        buffer.reset();
+        assert_eq!(None, buffer.get(0));
+
+        buffer.load::<StaticEncoder, 0>(&data);
+        assert_eq!(Some(0), buffer.get(0));
+    }
+
+    struct StaticEncoder;
+
+    impl Protocol for StaticEncoder {
+        type Cmd = Vec<u32>;
+    }
+
+    impl<const FREQ: u32> ProtocolEncoder<FREQ> for StaticEncoder {
+        type EncoderData = u32;
+
+        const DATA: Self::EncoderData = 0;
+
+        fn encode(cmd: &Self::Cmd, buf: &mut [u32]) -> usize {
+            for (dst, src) in buf.iter_mut().zip(cmd) {
+                *dst = *src;
+            }
+
+            usize::min(buf.len(), cmd.len())
         }
     }
 }
